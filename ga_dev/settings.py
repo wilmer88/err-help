606e27d8143c02a3a58570a92d3184cfc1051a53
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-10 18:28:33 -0400

    fixed url bug not allowing get method to database to find and retrive data.

--- ga_dev/settings.py
+++ ga_dev/settings.py
@@ -79,26 +79,26 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ga_dev.wsgi.application'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'wilmerbaby',
-#         'USER': 'postgres',
-#         'PASSWORD': 'morter706',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wilmerbaby',
+        'USER': 'postgres',
+        'PASSWORD': 'morter706',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 import dj_database_url
 # if IS_HEROKU: 
 #     DATABASE_URL='postgresql://<postgresql>'
