b2b5a6ae734b9130e8c763f103d6773feb085cdc
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-10 05:50:27 -0400

    new aws database

--- usarios/models.py
+++ usarios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 class ErrModel(models.Model):
     lang_name = models.CharField(max_length=240, blank=False, null=False)
     err_title = models.CharField(max_length=240, blank=False, null=False)
-    fixer_code = models.CharField(max_length=2000, blank=False, null=False)
+    fixer_code = models.CharField(max_length=3000, blank=False, null=False)
     os_tech = models.CharField(max_length=240,blank=False, null=False)   
     def __str__(self):
         """A human readible representation of user object"""
