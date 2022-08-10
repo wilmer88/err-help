501edaf1199711e67c36eb9d6189b7523884881f
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-10 02:18:51 -0400

    fixed database config

--- usarios/urls.py
+++ usarios/urls.py
@@ -3,7 +3,7 @@ from . import views
 #the $ sign besides home restrict the path from having home plus characters and still hit the home path
 urlpatterns = [
  re_path(r'^edit/(?P<id>[0-9]+)/$', views.edit, name="edit"),
-#  re_path(r'^delete/(?P<pk>[0-9]+)/$', views.delete, name="delete"),
+#  re_path(r'^delete/(?P<ic>[0-9]+)/$', views.delete, name="delete"),
  re_path(r'^$', views.index, name='index'),
  re_path(r'^index$', views.index, name='index'),
  re_path(r'^add$', views.add, name="add")
