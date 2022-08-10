a45540d42d138913d345e6716402806ae7ee32cd
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-10 18:38:18 -0400

    fixed url bug not allowing get method to database to find and retrive data.

--- ga_dev/settings.py
+++ ga_dev/settings.py
@@ -79,33 +79,43 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ga_dev.wsgi.application'
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+MAX_CONN_AGE = 600
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wilmerbaby',
-        'USER': 'postgres',
-        'PASSWORD': 'morter706',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'wilmerbaby',
+#         'USER': 'postgres',
+#         'PASSWORD': 'morter706',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 import dj_database_url
+if "DATABASE_URL" in os.environ:
+    # Configure Django for DATABASE_URL environment variable.
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
+
+    # Enable test database if found in CI environment.
+    if "CI" in os.environ:
+        DATABASES["default"]["TEST"] = DATABASES["default"]
+
 # if IS_HEROKU: 
 #     DATABASE_URL='postgresql://<postgresql>'
 # else:
 #     DATABASE_URL='sqlite:///'+ os.path.join(BASE_DIR,'db.sqlite3')    
 # DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 
