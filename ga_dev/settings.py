4111f9ad9346b6f9fa61fc03f05f030857c74cf9
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-10 01:56:56 -0400

    fixed database config

--- ga_dev/settings.py
+++ ga_dev/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'needed',
 #         'USER': 'wilmerbaby',
 #         'PASSWORD': 'ratachanga',
